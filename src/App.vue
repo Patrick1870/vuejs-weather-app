a490eb5671992d6f5e650d85a0a543a329e9d7fc
Patryk Adach <patrykadachofficial@gmail.com>
2021-04-16 00:10:55 +0200

    Updated main component

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,19 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Szukaj..." />
+        <input type="text" class="search-bar" placeholder="Szukaj..." v-model="query" @keypress="fetchWeather" />
+        {{ query }}
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Poznań, Polska</div>
+          <div class="location">{{weather.name}}, {{ weather.sys.country }}</div>
           <div class="date">Poniedziałek 15 kwietnia 2021</div>
         </div>
 
         <div class="weather-box">
-          <div class="temp">-9°</div>
-          <div class="weather">Snow</div>
+          <div class="temp">-{{Math.round(weather.main.temp)}}°c</div>
+          <div class="weather">{{weather.weather[0].main}}</div>
         </div>
       </div>
     </main>
@@ -25,8 +26,28 @@ export default {
   name: 'App',
   data () {
     return {
-      API_KEY: '67f4453eda315f562042678d9977265b'
+      api_key: '67f4453eda315f562042678d9977265b',
+      url_base: 'https://api.openweather.org/data/2.5/',
+      query: '',
+      weather: {}
     }
+  },
+  methods: {
+      fetchWeather(e) {
+        if (e.key == "Enter") {
+          fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)  
+            .then(res => {
+              return res.json();
+            }).then(this.setResults);
+        }
+      },
+      setResults (results) {
+        this.weather = results;
+      },
+    dateBuilder () {
+      
+    }
+
   }
 }
 </script>
@@ -116,5 +137,15 @@ main {
   background-color: rgba(255,255,255, 0.25);
   border-radius: 16px;
   margin: 30px 0px;
+
+  box-shadow: 3px 6px rgba(0,0,0,0.25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: 700;
+  text-shadow: 3px 6px rgba(0,0,0,0.25);
 }
 </style>
